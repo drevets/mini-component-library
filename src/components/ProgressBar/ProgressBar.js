38aa9b648b6ebae2e58e9d53c75b5cd01053fdfd
Emily Drevets <drevets@gmail.com>
2021-12-07 20:20:58 -0600

    copy paste Josh solution

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -42,7 +42,7 @@ const Progress = styled.div`
   }};
 `;
 
-const ProgressBar = ({ value, size }) => {
+const MyProgressBar = ({ value, size }) => {
   return (
     <ProgressWrapper size={size}>
       <Progress
@@ -52,9 +52,80 @@ const ProgressBar = ({ value, size }) => {
         aria-valuenow={value}
         size={size}
         value={value}
-      />
+      >
+        <VisuallyHidden>{value}%</VisuallyHidden>
+      </Progress>
     </ProgressWrapper>
   );
 };
 
-export default ProgressBar;
+export default MyProgressBar;
+
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
+  },
+  medium: {
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
+  },
+};
+
+const JoshProgressBar = ({ value, size }) => {
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
+  }
+
+  return (
+    <Wrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        "--padding": styles.padding + "px",
+        "--radius": styles.radius + "px",
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            "--width": value + "%",
+            "--height": styles.height + "px",
+          }}
+        />
+      </BarWrapper>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  /* Trim off corners when progress bar is near-full. */
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
