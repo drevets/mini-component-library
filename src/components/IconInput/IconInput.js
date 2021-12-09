0fae89fa08d2478a1d06c0c117ca441b8adba8e2
Emily Drevets <drevets@gmail.com>
2021-12-09 17:57:18 -0600

    josh icon component

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -82,3 +82,75 @@ const IconInput = ({ label, icon, width = 150, size, placeholder, id }) => {
 };
 
 export default IconInput;
+
+const STYLES = {
+  small: {
+    fontSize: 14,
+    iconSize: 16,
+    borderThickness: 1,
+    height: 24,
+  },
+  large: {
+    fontSize: 18,
+    iconSize: 24,
+    borderThickness: 2,
+    height: 36,
+  },
+};
+
+const JoshIconInput = ({ label, icon, width = 250, size, ...delegated }) => {
+  const styles = STYLES[size];
+
+  // TODO: validate size
+
+  return (
+    <JoshWrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <JoshIconWrapper style={{ "--size": styles.iconSize + "px" }}>
+        <Icon id={icon} size={styles.iconSize} />
+      </JoshIconWrapper>
+      <TextInput
+        {...delegated}
+        style={{
+          "--width": width + "px",
+          "--height": styles.height / 16 + "rem",
+          "--border-thickness": styles.borderThickness + "px",
+          "--font-size": styles.fontSize / 16 + "rem",
+        }}
+      />
+    </JoshWrapper>
+  );
+};
+
+const JoshWrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const JoshIconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+const TextInput = styled.input`
+  width: var(--width);
+  height: var(--height);
+  font-size: var(--font-size);
+  border: none;
+  border-bottom: var(--border-thickness) solid ${COLORS.black};
+  padding-left: var(--height);
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
